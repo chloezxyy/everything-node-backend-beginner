9b495ca7273ad1aa60d95daae461c931a6b32fd4
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-09-15 10:06:54 +0800

    16-streams.js

---
+++ node-express-course/16-streams.js
@@ -0,0 +1,16 @@
+// steams are used to read and write sequentially
+// steams extends the event emitter class
+// steams are useful for large amt of data, such as files/network connections where you dont want to load everything into memory
+
+const { createReadStream } = require('fs')
+
+// this means that we will read by chunks of 90000 bytes and the remaining of the file will be read in the last chunk
+const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf8' })
+
+stream.on('data', (result) => {
+    console.log(result)
+})
+
+stream.on('error', (err) => {
+    console.log(err)
+})
