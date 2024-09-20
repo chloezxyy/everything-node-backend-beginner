a94041e2a98383bedad94da72dd4a5cf5c95e465
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-09-20 15:50:20 +0800

    refactor data processing with reduce

--- weather-backend-node/index.js
+++ weather-backend-node/index.js
@@ -1,73 +1,61 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 
-// get the text file
-readFile("./data/weather_stations.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+async function processWeatherData() {
+  try {
+    const data = await readFile("./data/weather_stations.txt", "utf8");
+
+    // process data
+    const dataArray = JSON.stringify(data).split("\\n").slice(2);
+
+    // get the unique city and count
+    const cityTemp = dataArray.reduce((acc, cur) => {
+      const [city, temp] = cur.split(";");
+      const cityIndex = acc.findIndex((item) => item.city === city);
+      if (cityIndex === -1) {
+        acc.push({
+          city,
+          totalTemp: Number(temp),
+          count: 1,
+        });
+      } else {
+        acc[cityIndex].totalTemp += Number(temp);
+        acc[cityIndex].count += 1;
+      }
+      return acc;
+    }, []);
+
+    // get the av temp by city
+    /*
+    { city: 'Santiago', totalTemp: 29.245099999999994, count: 10 },
+    { city: 'Xingtai', totalTemp: 37.0659, count: 1 },
+    *
+    { city: 'Santiago', avTemp: 29.245099999999994/10 },
+    { city: 'Xingtai', avTemp: 37.0659 },
+    * */
+    const avTempByCity = cityTemp.reduce((acc, cur) => {
+      // iterate through the data and get the av temp
+      acc.push({
+        city: cur.city,
+        avTemp: cur.totalTemp / cur.count,
+      });
+      return acc;
+    }, []);
+
+    let content = "";
+    avTempByCity.forEach((data) => {
+      const line = `${data.city}: ${data.avTemp}\n`;
+      content += line;
+    });
+    return content;
+  } catch (e) {
+    console.error(e);
   }
+}
 
-  const dataArray = JSON.stringify(data).split("\\n");
-  const dataTemp = dataArray.slice(2, dataArray.length - 1); // remove the comments
-
-  const cityTemp = {}; // { Tokyo: 34}
-  const cityCount = {}; // { Tokyo: 4}
-  /*
-   * cityTemp: [{ Tokyo: 45.34, count: 3}]
-   * */
-  dataTemp.forEach((item) => {
-    const [city, temp] = item.split(";");
-
-    // built cityTemp
-    if (!cityTemp.hasOwnProperty(city)) {
-      cityTemp[city] = 0;
-    }
-    cityTemp[city] += Number(temp);
-
-    // built cityCount
-    if (!cityCount.hasOwnProperty(city)) {
-      cityCount[city] = 0;
-    }
-    cityCount[city] += 1;
-  });
-
-  // sort the cities alphabetically
-  const sortedKeysCityTemp = Object.keys(cityTemp).sort((a, b) => {
-    return a.localeCompare(b);
-  });
-  const sortedKeysCityCount = Object.keys(cityCount).sort((a, b) => {
-    return a.localeCompare(b);
-  });
-
-  // rebuild alphabetically
-  const sortedCityTemp = {};
-  sortedKeysCityTemp.forEach((key) => {
-    sortedCityTemp[key] = cityTemp[key];
-  });
-
-  const sortedCityCount = {};
-  sortedKeysCityCount.forEach((key) => {
-    sortedCityCount[key] = cityCount[key];
-  });
-
-  const averageCityTemp = {};
-  for (let i = 0; i < Object.keys(sortedCityTemp).length; i++) {
-    const city = Object.keys(sortedCityTemp)[i];
-    const temp = Object.values(sortedCityTemp)[i];
-    const count = Object.values(sortedCityCount)[i];
-    averageCityTemp[city] = temp / count;
-  }
-  console.log({ averageCityTemp });
-
-  // convert map into content before passing it into the writeFile method
-  const content = console.log("start writing file");
-  writeFile("./data/av_temp_in_cities", content, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("file written successfully");
-    // wrote the averageCityTemp into a file
-  });
-  console.log("done writing file");
-});
+async function writeWeatherData() {
+  try {
+    const weatherString = await processWeatherData();
+    console.log({ weatherString });
+  } catch (e) {}
+}
+writeWeatherData();
