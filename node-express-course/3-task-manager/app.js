0d9e6d115bc2d3291fd4709739d79ff42d61c5e3
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 11:31:59 +0800

    3-task-manager: 3:05:20 - Port Variable Setting

--- node-express-course/3-task-manager/app.js
+++ node-express-course/3-task-manager/app.js
@@ -17,7 +17,7 @@ app.use(errorHandlerMiddleware);
 // routes
 app.use("/api/v1/tasks", tasks);
 
-const port = "3000";
+const port = process.env.PORT || "3000";
 
 const start = async () => {
   try {
