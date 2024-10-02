3743459e2d21fa980c2a675c05eebc2bc51c2c98
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 14:28:54 +0800

    4-store-api: 3:41:30 -  Automating adding to database

---
+++ node-express-course/4-store-api/populate.js
@@ -0,0 +1,22 @@
+/*
+ * to seed the database with data
+ * */
+
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const Product = require("./models/product");
+const jsonProducts = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProducts); // dynamically create products
+    console.log("Success");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
