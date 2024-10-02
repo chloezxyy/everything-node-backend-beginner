a6064afe2e381f5be48daa112adb66e038bda380
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 10:50:30 +0800

    write files - 11, 12

--- node-express-course/1-node-tutorial/11-fs-async.js
+++ node-express-course/1-node-tutorial/11-fs-async.js
@@ -1,32 +1,35 @@
-
 // asynchronous
-const { readFile, writeFile } = require('fs');
+const { readFile, writeFile } = require("fs");
 
-console.log('start')
+console.log("start");
 // similar to addEventListener callback function
-readFile('./content/first.txt', 'utf-8', (err, result) => {
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf-8", (err, result) => {
     if (err) {
-        console.log(err)
-        return
+      console.log(err);
+      return;
     }
-    const first = result
-    readFile('./content/second.txt', 'utf-8', (err, result) => {
+    const second = result;
+
+    writeFile(
+      "./content/result-async.js.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
         if (err) {
-            console.log(err)
-            return
+          console.log(err);
+          return;
         }
-        const second = result
+        console.log("done with async.js task");
+      },
+    );
+  });
+});
 
-        writeFile('./content/result-async.txt', `Here is the result: ${first}, ${second}`, (err, result) => {
-            if (err) {
-                console.log(err)
-                return
-            }
-            console.log('done with async task')
-        })
-    })
-})
+console.log("starting next task");
 
-console.log('starting next task')
-
-// can write this code in a more readable way using promises, async/await
+// can write this code in a more readable way using promises, async.js/await
