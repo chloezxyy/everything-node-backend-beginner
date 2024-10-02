8da2dc63644c89da0308b70bd5e26adabf6fba74
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 11:27:39 +0800

    3-task-manager: 1:16:20 - Mongoose Schema

---
+++ node-express-course/3-task-manager/models/Task.js
@@ -0,0 +1,19 @@
+/*
+ * Mongoose schema and model for Task collection in the MongoDB database*/
+
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
