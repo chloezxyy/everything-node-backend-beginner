5cec9cbd3057ee7e9a081ac730921db90472d0d6
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 21:42:22 +0800

    4-task-manager: 3:51:30 -  Mongoose Filter Methods

--- node-express-course/4-store-api/controllers/products.js
+++ node-express-course/4-store-api/controllers/products.js
@@ -1,15 +1,35 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const product = await Product.find({
-    name: "vase table",
-    featured: true,
+  const search = "e";
+  const products = await Product.find({
+    name: {
+      $regex: search,
+      $options: "i",
+    },
+    company: "ikea",
+    featured: false,
   });
-  res.status(200).json({ product, nHits: product.length });
+  res.status(200).json({ products, nHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true";
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log({ queryObject });
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
