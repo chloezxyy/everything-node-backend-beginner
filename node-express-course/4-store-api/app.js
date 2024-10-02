e163ffd93ba9abf10ffcb4f1da41972916a52838
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-02 13:49:09 +0800

    4-store-api: project setup (express, db, controllers and routes)

---
+++ node-express-course/4-store-api/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+const connectDb = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log("Server is listening on port 3000...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
