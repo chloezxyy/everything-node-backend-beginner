5a2163695bad0aee6bae4f8b4515cc573ff8fb01
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-10-08 14:34:38 +0800

    5-jwt-basics: 5:32:30 -  Jsonwebtoken Explanation and Setup

---
+++ node-express-course/5-jwt-basics/controllers/main.js
@@ -0,0 +1,46 @@
+// check username, password in post (login) request
+// if exists create new JWT
+// send back to frontend
+
+// setup authentication so only the request with JWT can access the /dashboard route
+
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  // 3 options to check
+  // 1. mongoose validation - db validation
+  // 2. sits infront of our request - joi
+  // 3. check in the controller
+
+  // just for demo, usually provided by db
+  const id = new Date().getDate();
+
+  // try to keep the payload small - for better ux, and dont include sensitive data!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
+};
+
+// where we want to share our secret/authorized data
+const dashboard = async (req, res) => {
+  console.log(req.headers);
+  const luckyNumber = Math.random() * 99;
+  res.status(200).json({
+    msg: `Hellow, John Doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
